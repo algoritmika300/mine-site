a432180ccad943a09a6f8808803e2f377e1ee799
algoritmika300 <algoritmika300@gmail.com>
2023-07-31 17:16:09 +0600

    test1

---
+++ script.js
@@ -0,0 +1,36 @@
+function scrollDown() {
+    window.scrollTo({top:window.innerHeight, behavior:'smooth'});
+}
+
+let button = document.querySelector('.header_btn')
+
+button.addEventListener('click', scrollDown)
+
+anime (
+    {
+        tagrets: '.header_left h1',
+        opacity: 1,
+        duration: 500,
+        easing: 'linear'
+    }
+)
+
+anime (
+    {
+        tagrets: '.header_left p',
+        opacity: 1,
+        duration: 500,
+        delay: 200,
+        easing: 'linear'
+    }
+)
+
+anime (
+    {
+        targets: '.header_btn',
+        translateY: [-window.innerHeight/2, 0],
+        duration: 300,
+        easing: 'easeOutQuad'
+    }
+)
+
